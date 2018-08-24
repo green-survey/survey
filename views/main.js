0f035d92562239901837ae17e1ea7c6d2f35a44f
zhouhancheng <z308114274@gmail.com>
2018-08-24 11:31:47 +0800

    .

--- views/main.js
+++ views/main.js
@@ -1,158 +1,191 @@
 var html = require('choo/html')
+var css = require('sheetify')
+var Nanocomponent = require('choo/component')
+var surveys = require('../survey.json')
+var mitt = require('mitt')
+var emy = mitt()
 
-var TITLE = 'survey - main'
+var TITLE = '环保知识问卷'
+var result = [null, null, null, null, null, null, null]
+var answer = [1, 1, 0, 2, 1, 2, 3]
+
+css('../styles')
 
 module.exports = view
 
+class Question extends Nanocomponent {
+  constructor (data) {
+    super()
+    this.title = data.title
+    this.options = data.options
+    this.id = data.id
+    this.handleClick = this.handleClick.bind(this)
+    this.currentId = null
+  }
+
+  createElement () {
+    return html`
+      <li>
+        <p class="mv2">${this.title}?</p>
+        <form id="q1">          
+            ${this.options.map((option, i) => {
+              return html`
+                <div class="flex mb2">
+                  <i
+                  onclick=${this.handleClick(this.id, i)}
+                  class='${this.showResult ? '' : 'bg-white br2 b--blue ba bw015'} w12 h12 flex items-center justify-center mr1'>
+                  ${this.currentId === i ? 
+                    html`<i class='icon ${this.showResult ? (this.currentId === answer[this.id] ? "icon_agree" : "icon_wrong"): "icon_agree"}'></i>` : 
+                    html`<i class='icon ${this.showResult ? (i === answer[this.id] ? "icon_agree" : ""): ""}'></i>`}
+                  </i>              
+                  <span 
+                  onclick=${this.handleClick(this.id, i)}
+                  class="items-center justify-center flex f5 ml1">
+                  ${(i + 1) + "." + option}
+                  </span>
+                </div> 
+              `
+            })}                         
+        </form>
+      </li>
+    `
+  }  
+
+  load () {
+    emy.on('showResult', () => {
+      this.showResult = true
+      this.render()
+    })
+  }
+
+  handleClick (id, subId) {
+    return e => {
+      if (this.showResult) {
+        return 
+      }
+
+      this.currentId = subId
+      result[id] = subId
+      this.render()
+    }
+  }  
+
+  update () {
+    return true
+  }  
+}
+
+class SButton extends Nanocomponent {
+  constructor (data) {
+    super()
+  }
+
+  createElement (destroy) {
+    if (destroy) {
+      return html`
+        <div class="w-100 flex mb2">
+        </div>
+      `
+    } else {
+      return html`
+        <div class="w-100 flex mb2">
+        
+          <section 
+            onclick=${this.handleClick()}
+            class="f5 br2 ba ph3 pv2 mb2 dib gold pointer grow ml4 center">
+            查看答案
+          </section>
+        </div>
+      `
+    }
+
+  }
+
+  handleClick () {
+    return e => {
+      window.scrollTo(0, 0)      
+      emy.emit('showResult')
+      this.render(true)
+    }
+  }
+
+  update () {
+    return true
+  }    
+}
+
+class Advertise extends Nanocomponent {
+  constructor () {
+    super()
+  }  
+
+  createElement () {
+    if (this.showResult) {
+      return html`
+        <div>
+          <p class='w-90 center dark-green'>
+          Hi!      
+          </p>          
+          <p class='w-90 center dark-green'>
+            我是一个普通的程序员，曾经在江西绿色之光环保组织做过志愿者，这个问卷就是在下做的。       
+          </p>      
+          <p class='w-90 center dark-green'>
+            为了给垃圾分类的活动作准备，我正在做厨余堆肥实验。          
+          </p> 
+          <p class='w-90 center dark-green'>
+            想想那些西瓜皮能变成肥料回馈土地，就觉得很美好。          
+          </p>   
+          <p class='w-90 center dark-green'>
+            我正在合肥开展环保活动，欢迎联系！          
+          </p>                                  
+          <p class='w-90 center dark-green'>微信：</p>
+          <img class='w-100' src='../assets/qrcode.png' />
+          <img class='w-100' src='../assets/p1.png' />
+          <img class='w-100' src='../assets/p2.png' />
+          <img class='w-100' src='../assets/p3.png' />
+          <img class='w-100' src='../assets/p4.JPG' />
+        </div>
+      `
+    } else {
+      return html`<div></div>`
+    }
+  } 
+
+  load () {
+    emy.on('showResult', () => {
+      this.showResult = true
+      this.render()
+    })    
+  }
+  
+  update () {
+    return true
+  }   
+}
+
 function view (state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
-
+  var sButton = new SButton()
+  var advertise = new Advertise()
   return html`
-    <body class="code lh-copy">
-      <main class="pa3 cf center">
-        <section class="fl mw6 w-50-m w-third-l pa3">
-          <h2>1.</h2>
-          <p>
-            Welcome to your new Choo application.
-            We're very happy you've made it this far.
-          </p>
-
-          <p>
-            You're now in control of your own Choo app. The moment you decide to
-            deploy it, it'll work offline and on any device.
-          </p>
-
-          <br>
-        </section>
-
-        <section class="fl mw6 w-50-m w-third-l pa3">
-          <h2>2.</h2>
-
-          <p>
-            We've outfitted your project with a small selection of commands to
-            help you achieve results faster:
-          </p>
-
-          <ul>
-            <li class="mb3">
-              <strong>npm start</strong><br>
-              start your project for local development.
-            </li>
-            <li class="mb3">
-              <strong>npm run build</strong><br>
-              compile your project for production.
-            </li>
-            <li class="mb3">
-              <strong>npm run inspect</strong><br>
-              visualize your project's dependencies.
-            </li>
-            <li class="mb3">
-              <strong>npm run create</strong><br>
-              scaffold a new file.
-            </li>
-          </ul>
-
-          <br>
-        </section>
-
-        <section class="fl mw6 w-50-m w-third-l pa3">
-          <h2>3.</h2>
-
-          <p>
-            Your project also comes with a few directories. These names have
-            special meanings for the build tool, so it's good to know what they
-            do.
-          </p>
-
-          <ul>
-            <li class="mb3">
-              <strong>assets/</strong><br>
-              Static files that can be served up, such as images and fonts.
-            </li>
-            <li class="mb3">
-              <strong>components/</strong><br>
-              Reusable fragments that can be composed into views.
-            </li>
-            <li class="mb3">
-              <strong>stores/</strong><br>
-              Pieces of logic that are shared by multiple components.
-            </li>
-            <li class="mb3">
-              <strong>views/</strong><br>
-              Combinations of components that are mapped to routes.
-            </li>
-          </ul>
-
-          <br>
-        </section>
-
-        <section class="fl mw6 w-50-m w-third-l pa3">
-          <h2>4.</h2>
-
-          <p>
-            So far we've provided you with one base view, <a
-            href="/oh-no">one fallback view</a>, and one store. This serves
-            as an example. A place to start from. It's your project now, so
-            go ahead and delete them once you know how they work.
-          </p>
-
-          <p>Number of clicks stored: ${state.totalClicks}</p>
-
-          <button class="dim ph3 ba bw1 pv2 b--black pointer bg-white"
-            onclick=${handleClick}>
-            Emit a click event
-          </button>
-
-          <br><br>
-        </section>
-
-        <section class="fl mw6 w-50-m w-third-l pa3">
-          <h2>5.</h2>
-
-          <p>
-            To make your development journey more pleasant, we've also
-            included <a
-            href="https://github.com/choojs/choo-devtools">devtools</a>. If
-            you open your browser console, here's a selection of the
-            commands that are at your disposal:
-
-            <ul>
-              <li class="mb3">
-                <strong>choo.state</strong><br>
-                Log the current application state.
-              </li>
-              <li class="mb3">
-                <strong>choo.log</strong><br>
-                Log the last 150 events received by the event bus.
-              </li>
-              <li class="mb3">
-                <strong>choo.emit</strong><br>
-                Emit an event inside the application event bus.
-              </li>
-              <li class="mb3">
-                <strong>choo.help</strong><br>
-                See an overview of all available commands.
-              </li>
-            </ul>
-          </p>
-        </section>
-
-        <section class="fl mw6 w-50-m w-third-l pa3">
-          <h2>6.</h2>
-
-          <p>
-            And that's about it! Thanks for reading. If you have any
-            questions, check out the <a  href="https://choo.io">docs</a> or reach
-            out on <a href="https://github.com/choojs/choo">GitHub</a> or <a
-            href="https://www.irccloud.com/irc/freenode/channel/choo">IRC</a>.
-            We're online everyday, and always around to help. Happy hacking!
-          </p>
-        </section>
+    <body class="code lh-copy bg-washed-blue">
+      <main class="pa1 cf center">
+        <section class="fl mw6 w-100 w-third-l tc">
+          <h2 class="tc dark-green mv3">${TITLE}</h2>
+        </section>  
+        <ol class="fl mw6 mt0 w-90 w-third-l dark-blue">
+          ${surveys.map((survey, id) => {
+            survey.id = id
+            var question = new Question(survey)
+            return question.render()
+          })}
+        </ol>  
+        ${sButton.render()}   
+        ${advertise.render()}                 
       </main>
+      
     </body>
   `
-
-  function handleClick () {
-    emit('clicks:add', 1)
-  }
 }
+
+
